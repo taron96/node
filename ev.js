6e3d9b0c57602784beedcb08fa77ba60d02ca6aa
taron96 <antonyantaron1996@gmail.com>
2017-05-18 20:44:05 +0400

    process.stdin.on()

--- ev.js
+++ ev.js
@@ -1,10 +1,14 @@
 'use strict';
 
-let counter = 0;
+// let counter = 0;
+//
+// const interval = setInterval(function() {
+//   console.log(`Called ${++counter} times`);
+//   if (counter === 7) {
+//     clearInterval(interval);
+//   }
+// }, 1000);
 
-const interval = setInterval(function() {
-  console.log(`Called ${++counter} times`);
-  if (counter === 7) {
-    clearInterval(interval);
-  }
-}, 1000)
+process.stdin.on('data', function(d) {
+  console.log(d.toString().toUpperCase());
+});
