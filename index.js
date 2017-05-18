be47e70c26a9348f5583e8713f3c62e3086879eb
taron96 <antonyantaron1996@gmail.com>
2017-05-18 19:12:36 +0400

    for of

--- index.js
+++ index.js
@@ -1,7 +1,11 @@
 'use strict';
 
-function speak() {
-  console.log('sdfsd');
+const speak = function() {
+  const arr = ['foo', 'bar', 'baz'];
+  for (const i of arr) {
+    console.log(i.toUpperCase());
+  }
+
 }
 
 speak();
